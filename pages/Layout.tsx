5396a2de8883d38c85360dd61cdbc14975b2e407
PaudelSworup <sworupkc9@gmail.com>
2023-11-06 22:22:13 +0545

    layout

--- pages/Layout.tsx
+++ pages/Layout.tsx
@@ -6,6 +6,8 @@ export default function Layout(props: Layout) {
     <div>
       <Head>
         <title>{props.title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       {props.children}
     </div>
