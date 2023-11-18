0234b5c1fd934fc20aef4a76b2d8bd53e19237d4
PaudelSworup <sworupkc9@gmail.com>
2023-11-18 21:24:02 +0545

    footers

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,8 +14,8 @@ const Footer = () => {
       <div className="w-full mx-auto  p-4 md:flex md:items-center md:justify-between">
         <span className="text-lg text-white sm:text-center dark:text-gray-400">
           © 2023
-          <Link href="/" className="hover:underline"></Link>. All Rights
-          Reserved.
+          <Link href="/" className="hover:underline"></Link>.Shree Krishna
+          Pranami Sampradaya.All Rights Reserved.
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-lg font-medium text-white sm:mt-0">
           <li>
@@ -36,24 +36,6 @@ const Footer = () => {
         </ul>
       </div>
     </footer>
-
-    // <div
-    //   style={{
-    //     background: "url('https://shorturl.at/fqxJW')",
-    //     backgroundSize: "cover",
-    //     filter: "grayscale(100%)",
-    //   }}
-    //   classNameName="flex sm:px-10 px-2 text-white tracking-widest font-serif h-28  justify-between items-center"
-    // >
-    //   <div classNameName="flex">
-    //     <FaCopyright classNameName="text-2xl mt-1" />
-    //     <h2 classNameName="sm:text-xl text-sm text-white">
-    //       Shree Krishna Pranami Sampradaya.
-    //     </h2>
-    //   </div>
-
-    //   <h2 classNameName="sm:text-xl text-sm">Developed by-Sworup kc</h2>
-    // </div>
   );
 };
 
